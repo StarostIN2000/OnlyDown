21737727b0c48220e44f3673d445a2dc71c76f03
StarostIN2000 <72708700+StarostIN2000@users.noreply.github.com>
2024-08-05 21:41:25 +0300

    muchacho

---
+++ OnlyDovn/Assets/c# scripts/FPSlock.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSlock : MonoBehaviour
+{
+    [SerializeField] int targetFrameRate = 60;
+    private void Awake()
+    {
+        Application.targetFrameRate = targetFrameRate;
+    }
+}
