8f687458b33b9e99e4c17458c309bd4a8e41f1f7
StarostIN2000 <72708700+StarostIN2000@users.noreply.github.com>
2024-08-11 16:43:19 +0300

    settings

--- OnlyDovn/Assets/_contentus/c# scripts/MainMenu.cs
+++ OnlyDovn/Assets/_contentus/c# scripts/MainMenu.cs
@@ -5,6 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] GameObject mainMenu;
+    [SerializeField] GameObject optionsMenu;
+
+    private void Awake()
+    {
+        mainMenu.SetActive(true);
+        optionsMenu.SetActive(false);
+    }
     public void PlayGame() 
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
